468d7d5af988a8faa929895eb373094b0e12c459
Egor Voynov <eavoynov@gmail.com>
2026-01-19 16:12:20 +0300

    Add class-based test for Circle class

---
+++ tests/test_circle.py
@@ -0,0 +1,34 @@
+import pytest
+import math
+import sys
+sys.path.append("/home/egor/Python/pytest_introspection/source")
+import source.shapes as shapes
+
+class TestCircle:
+
+    def setup_method(self, method):
+        print(f"Setting up {method}")
+        circle = shapes.Circle(3)
+        self.circle = circle
+
+    def test_radius_one(self):
+        self.circle.radius = 1
+        print(f"Circle's radius(it've changed to 1) equals to {self.circle.radius}")
+        assert self.circle.radius == 1
+
+
+    def test_radius_two(self):
+        print(f"Circle's radius(By default equals 3,"
+              f"but it've changed to 1 in time of previous method call)"
+              f"equals to {self.circle.radius}")
+        assert self.circle.radius == 3
+
+    def test_area(self):
+        assert self.circle.area() == math.pi * self.circle.radius ** 2
+    
+    def test_perimetr(self):
+        assert self.circle.perimetr() == 2 * self.circle.radius * math.pi
+
+    def teardown_method(self, method):
+        print(f"Tearing down {method}")
+        del self.circle
