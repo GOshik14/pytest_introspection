e2eb2448815451142296e4c4952bb014cba45249
Egor Voynov <eavoynov@gmail.com>
2026-01-19 15:22:20 +0300

    Add class Circle implementing the same named geometry figure

--- source/shapes.py
+++ source/shapes.py
@@ -7,3 +7,14 @@ class Shape:
     def perimetr(self):
         pass
 
+
+class Circle(Shape):
+
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius ** 2
+    
+    def perimetr(self):
+        return 2 * math.pi * self.radius
