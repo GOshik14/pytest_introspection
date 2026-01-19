7bec3b6f33e95cb8d8c275c9f2de265f8e6d53f4
Egor Voynov <eavoynov@gmail.com>
2026-01-19 23:25:10 +0300

    Add parametrize tests for class Square

--- tests/test_square.py
+++ tests/test_square.py
@@ -16,3 +16,21 @@ class TestSquare:
 
     def teardown_method(self):
         del self.Square  # just for studying purpose
+
+
+### Function-based tests with pytest parametrize decorator ###
+@pytest.mark.parametrize("length, expected_area", [
+    (3, 9),
+    (4, 16),
+    (5, 25)
+])
+def test_square_area(length, expected_area):
+    assert shapes.Square(length).area() == expected_area
+    
+@pytest.mark.parametrize("length, expected_perimetr", [
+    (3, 12),
+    (4, 16),
+    (5, 20)
+])
+def test_square_perimetr(length, expected_perimetr):
+    assert shapes.Square(length).perimetr() == expected_perimetr
