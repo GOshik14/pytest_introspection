fd6e46cdaaf259b36c3b885d72f3149ec31bbf03
Egor Voynov <eavoynov@gmail.com>
2026-01-19 16:31:58 +0300

    Add marks "generated_exception" to my_function tests with the specified functionality

--- tests/test_my_function.py
+++ tests/test_my_function.py
@@ -24,35 +24,44 @@ def test_divide_positive(a, b, expected):
     assert my_funcs.divide(4, 2) == 2
 
 #### Catching exceptions tests ####
+
+@pytest.mark.generated_exception
 def test_add_catch_exception_int_str():
     with pytest.raises(ValueError):
         my_funcs.add(1, "1")
 
+@pytest.mark.generated_exception
 def test_add_catch_exception_int_float():
     with pytest.raises(ValueError):
         my_funcs.add(1, 1.)
 
+@pytest.mark.generated_exception
 def test_add_catch_exception_str_float():
     with pytest.raises(ValueError):
         my_funcs.add("1", 1.)
 
 
+@pytest.mark.generated_exception
 def test_divide_catch_value_exception_int_str():
     with pytest.raises(ValueError):
         my_funcs.divide(1, "1")
 
+@pytest.mark.generated_exception
 def test_divide_catch_value_exception_int_float():
     with pytest.raises(ValueError):
         my_funcs.divide(1, 1.)
 
+@pytest.mark.generated_exception
 def test_divide_catch_value_exception_str_float():
     with pytest.raises(ValueError):
         my_funcs.divide("1", 1.)
 
+@pytest.mark.generated_exception
 def test_divide_catch_arithm_exception_0_0():
     with pytest.raises(ArithmeticError):
         my_funcs.divide(0, 0)
 
+@pytest.mark.generated_exception
 def test_divide_catch_arithm_exception_1_0():
     with pytest.raises(ArithmeticError):
         my_funcs.divide(1, 0)
